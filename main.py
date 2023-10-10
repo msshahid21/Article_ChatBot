83de75e31461eccacfee17f4ba24200d2391b0c8
msshahid21 <salman@pop-os.localdomain>
2023-10-09 23:31:24 -0400

    Adding Button Functionality for Sidebare to Load and Split Data, Create Embeddings and Save to FAISS Index, and Save the Docs to a Pickle File

--- main.py
+++ main.py
@@ -20,10 +20,30 @@ st.title("News Research Tool")
 ## Streamlit Sidebar
 st.sidebar.title("News Article URLs")
 
+urls = []
 for i in range(3):
-    st.sidebar.text_input(f"URL {i + 1}")
+    url = st.sidebar.text_input(f"URL {i + 1}")
+    urls.append(url)
 
 process_url_clicked = st.sidebar.button("Process URLs")
 
 if process_url_clicked:
-    pass
+    # Loading Data from User-Inputted URLs
+    loader = UnstructuredURLLoader(urls = urls)
+    data = loader.load()
+
+    # Spltting Data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators = ['\n\n', '\n', '.', ','],
+        chunk_size = 1000
+    )
+    docs = text_splitter.split_documents(data)
+
+    # Create Embeddings and Save it to FAISS Index
+    embeddings = OpenAIEmbeddings()
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
+
+    # Save the FAISS Index to a Pickle File
+    file_path = "faiss_store_openai.pkl"
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore_openai, f)
