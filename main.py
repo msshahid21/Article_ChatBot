d54529f23c788c316f39c29bca40458c92c9542d
msshahid21 <salman@pop-os.localdomain>
2023-10-09 23:14:53 -0400

    Streamlit Sidebar layout

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ from dotenv import load_dotenv
 # Importing OpenAPI Enviroment Key
 from secret_key import api_key
 os.environ['OPENAI_API_KEY'] = api_key
+
+# Setting Up Streamlit Interface
+st.title("News Research Tool")
+
+## Streamlit Sidebar
+st.sidebar.title("News Article URLs")
+
+for i in range(3):
+    st.sidebar.text_input(f"URL {i + 1}")
+
+process_url_clicked = st.sidebar.button("Process URLs")
+
+if process_url_clicked:
+    pass
