586d7093a848f9029c6b2a197d58ee5df0c99a23
msshahid21 <salman@pop-os.localdomain>
2023-10-09 23:09:52 -0400

    Importing Libraries and OpenAI API Key

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+
+# Importing OpenAPI Enviroment Key
+from secret_key import api_key
+os.environ['OPENAI_API_KEY'] = api_key
